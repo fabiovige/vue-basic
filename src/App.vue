3d6aad32c38cbfc4e365633544f855f00877950a
Fabio Vige <fabiovige@gmail.com>
2023-05-24 08:23:06 -0300

    reatividade e listagem dinamica

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <h1>App</h1>
     <h2>{{ userName }}</h2>
-    <div v-if="showHeader">
+  <button v-on:click="showHeader = !showHeader">
+    <template v-if="!showHeader"> abrir</template>
+    <template v-else> fechar</template>
+  </button>
+  <div v-if="showHeader">
         <Header></Header>
+      <img :src="img" alt="">
     </div>
-    <button v-on:click="showHeader = !showHeader"> show header</button>
+
     <Menu></Menu>
   <button @click="count++"> Add </button>
   {{ count }}
+
+  <h3>Profile</h3>
+  <ul>
+    <li v-for="(value, key) in user" key="{{ key }}">{{ key }} - {{ value }}</li>
+  </ul>
+
+
+
+  <h3>User list</h3>
+  <ul>
+    <template v-for="(user) in users" :key="user.id">
+      <li v-if="user.is_admin === 1">{{ user.firstName }} - {{ user.age }}</li>
+    </template>
+  </ul>
 
   <RouterView />
 
@@ -15,8 +34,9 @@
 </template>
 
 <script setup>
-
-import {onUpdated, onMounted, ref} from "vue";
+// ref somente valores primitivos "strings, int, bool, undefined, etc"
+// reactive somente para objetos exemplo: array [] e objetos {}
+import {onUpdated, onMounted, ref, reactive} from "vue";
 
   import Menu from '@/components/Menu.vue'
   import Footer from '@/components/Footer.vue'
@@ -24,29 +44,57 @@ import {onUpdated, onMounted, ref} from "vue";
 
   const count = ref(0);
   const showHeader = ref(false);
-  const userName = ref('')
-  const users = ref([])
+  const userName = ref('');
+  const img = ref('https://picsum.photos/200/300');
+
+  const user = reactive({
+    id: 1,
+    firstName: 'Fabio Vige',
+    age: 47,
+    is_admin: 1
+  });
+
+  const users = reactive([
+    {
+      id: 1,
+      firstName: 'Fabio Vige',
+      age: 47,
+      is_admin: 1
+    },
+    {
+      id: 2,
+      firstName: 'Valéria Nunes',
+      age: 41,
+      is_admin: 1
+    },
+    {
+      id: 3,
+      firstName: 'Isabela Nunes',
+      age: 23,
+      is_admin: 0
+    },
+    {
+      id: 4,
+      firstName: 'Gustavo Nunes',
+      age: 21,
+      is_admin: 0
+    },
+    {
+      id: 5,
+      firstName: 'Eduardo Nunes',
+      age: 17,
+      is_admin: 0
+    },
+  ]);
 
   onMounted(() => {
       console.log("onMounted");
-      users.value = [
-          {
-              firstName: 'Isabela Nunes',
-              age: 23
-          },
-          {
-              firstName: 'Gusravo Nunes',
-              age: 21
-          },
-          {
-              firstName: 'Eduardo Nunes',
-              age: 17
-          },
-      ]
+      console.log('count ' + count.value);
   })
 
   onUpdated(()=>{
     console.log("atualiza algum componente no App");
+    console.log('count ' + count.value);
   })
 
 </script>
